886174d1003508da0d0a302c876dae0880d55f85
AdamSkuse <a.t.skuse@gmail.com>
2017-05-23 18:54:13 -0700

    Adds functions for each key

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,32 +2,41 @@ var buffer = []
 
 $('.button').on('click', function(){
   var input = ($(this).attr('data-key'));
-  console.log(input);
-  if (/[0-9]/.test(input)) {
-    numberHandler(); 
+    if (/[0-9]/.test(input)) {
+    numberHandler(input); 
   } else if (/[/\+\*-]/.test(input)) {
-    operatorHandler();
+    operatorHandler(input);
   } else if (/[=]/.test(input)) {
     equalsHandler();
   } else if (/(AC|C)/.test(input)) {
     functionHandler();
   } else {
-    decimalHandler();
+    decimalHandler(input);
   }
 });
 
-function numberHandler() {
+function numberHandler(digit) {
   // deals with numbers being input
-  console.log('digit');
+  console.log('digit: ' + digit);
+  buffer.push(digit);
+  console.log('buffer: ' + buffer)
 }
 
-function decimalHandler() {
+function decimalHandler(decimal) {
   // deals with decimal point being input
-  console.log('decimal point');
+  console.log('decimal point: ' + decimal);
+  buffer.push(decimal);
+  console.log('buffer: ' + buffer)
 }
 
-function operatorHandler() {
-  console.log('operator');
+function operatorHandler(operator) {
+  console.log('operator: ' + operator);
+  buffer.push(operator);
+  console.log('buffer: ' + buffer)
+  
+  // edge case: buffer is empty
+  // edge case: last entry in buffer is a decimal point
+  // edge case: last entry in buffer is another operator
 }
 
 function equalsHandler() {
@@ -37,5 +46,15 @@ function equalsHandler() {
 
 function functionHandler() {
   //deals with function buttons being pressed
+  //if function is AC, reset buffer and display
+  //if function is clear...delete last item from buffer?
+  
   console.log('function');
+}
+
+function updateDisplay() {
+  // if last entry was digit or decimal point, add it to display
+  // if last entry was operator, clear display
+  // if last entry was equals, display answer
+  
 }
